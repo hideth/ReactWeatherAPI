52aafd7037319e93e32bb8488a6287e351ca322f
h1d3th <razider10@gmail.com>
2021-01-06 15:47:45 +0100

    added clickable map added doctrine to the project

--- assets/js/components/Home.js
+++ assets/js/components/Home.js
@@ -1,37 +1,34 @@
 // ./assets/js/components/Home.js
-    
-import React, {Component} from 'react';
-import {Route, Switch,Redirect, Link, withRouter} from 'react-router-dom';
+
+import React, { Component } from 'react';
+import { Route, Switch, Redirect, Link, withRouter } from 'react-router-dom';
 import Users from '../components/User';
 import Posts from '../components/Post';
 import Map from '../components/Map';
-    
+
 class Home extends Component {
-    
+
     render() {
         return (
-           <div>
-               <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                   <Link className={"navbar-brand"} to={"/"}> Map </Link>
-                   <div className="collapse navbar-collapse" id="navbarText">
-                       <ul className="navbar-nav mr-auto">
-                           <li className="nav-item">
-                               <Link className={"nav-link"} to={"/posts"}> History </Link>
-                           </li>
-                       </ul>
-                   </div>
-               </nav>
-               <div>
-                   <Map></Map>
-               </div>
-               <Switch>
-                   <Redirect exact from="/" to="/users" />
-                   <Route path="/users" component={Users} />
-                   <Route path="/posts" component={Posts} />
-               </Switch>
-           </div>
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <Link className={"navbar-brand"} to={"/"}> Map </Link>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link className={"nav-link"} to={"/users"}> History </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <Switch>
+                   <Redirect exact from="/" to="/map" />
+                    <Route path="/map" component={Map} />
+                    <Route path="/users" component={Users} />
+                </Switch>
+            </div>
         )
     }
 }
-    
+
 export default Home;
--- assets/js/components/Map.js
+++ assets/js/components/Map.js
@@ -5,11 +5,13 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const defaultProps = {
   center: {
-    lat: 59.95,
-    lng: 30.33
+    lat: 52.31,
+    lng: 15.01,
   },
-  zoom: 4
+  zoom: 3,
 };
+
+const _onClick = ({x, y, lat, lng, event}) => console.log(x, y, lat, lng, event)
 class SimpleMap extends Component {
   render() {
     return (
@@ -18,12 +20,8 @@ class SimpleMap extends Component {
           bootstrapURLKeys={{ key:'AIzaSyBRjeVSxqw12aExgf0sZvfXoGRJERqIlMs' }}
           defaultCenter={defaultProps.center}
           defaultZoom={defaultProps.zoom}
+          onClick={_onClick}
         >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
         </GoogleMapReact>
       </div>
     );
