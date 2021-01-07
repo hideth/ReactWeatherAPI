2b6feb2fc0f2593ae7fe592ee92f5e6cece47592
h1d3th <razider10@gmail.com>
2021-01-07 11:50:08 +0100

    loading state update on axios request

--- assets/js/components/Map.js
+++ assets/js/components/Map.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import axios from 'axios';
+import ReactLoading from 'react-loading';
 
 const defaultProps = {
   center: {
@@ -11,40 +12,44 @@ const defaultProps = {
   apiKey: 'AIzaSyBRjeVSxqw12aExgf0sZvfXoGRJERqIlMs',
 };
 
-function request(lat, lng) {
-  let params = {
-    lat: lat,
-    lng: lng,
-  }
-  let url = '/api/weather/add';
-  axios.post(url, params, {
-    "headers": {
-
-      "content-type": "application/json",
-
-    },
-
-  })
-    .then(function (response) {
-      console.log(response);
-    })
-}
-const _onClick = ({ x, y, lat, lng, event }) => request(lat, lng)
 class SimpleMap extends Component {
   constructor() {
     super();
+    this.state = { loading: false }
+    this.request = this.request.bind(this);
+  }
+  
+  request(props) {
+    let params = {
+      lat: props.lat,
+      lng: props.lng,
+    }
+    let url = '/api/weather/add';
+    this.setState({ loading: true });
+    var self = this;
+    axios.post(url, params, {
+      "headers": {
+        "content-type": "application/json",
+      },
 
-    this.state = { posts: [], loading: true }
+    })
+      .then(function (response) {
+        self.setState({ loading: false });
+        console.log(response);
+      })
   }
 
   render() {
     return (
       <div style={{ height: '100vh', width: '100%' }}>
+        <div style={{ position: 'absolute', top: '50%', left: '50%', zIndex: 10 }}>
+          {this.state.loading ? <ReactLoading color='black' /> : <div></div>}
+        </div>
         <GoogleMapReact
           bootstrapURLKeys={{ key: defaultProps.apiKey }}
           defaultCenter={defaultProps.center}
           defaultZoom={defaultProps.zoom}
-          onClick={_onClick}
+          onClick={this.request}
         >
         </GoogleMapReact>
       </div>
